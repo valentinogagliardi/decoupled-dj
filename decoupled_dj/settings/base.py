e7bad8e00890ab063b31f1d925d8cbcf28a0e8ab
valentinogagliardi <valentino@valentinog.com>
2021-02-02 17:34:59 +0100

    Configure authentication and permission classes for DRF

--- decoupled_dj/settings/base.py
+++ decoupled_dj/settings/base.py
@@ -83,3 +83,10 @@ USE_TZ = True
 STATIC_URL = env("STATIC_URL")
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAdminUser"],
+}
