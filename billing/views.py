f08bbdc1dd5de35ab7e055ef67845db548001203
valentinogagliardi <valentino@valentinog.com>
2021-02-02 13:47:55 +0100

    Require authentication for the TemplateView

--- billing/views.py
+++ billing/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class Index(TemplateView):
+class Index(LoginRequiredMixin, TemplateView):
     template_name = "billing/index.html"
