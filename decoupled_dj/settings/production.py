b4568793df334ae3a8e9ab0eb073b65065eefc70
valentinogagliardi <valentino@valentinog.com>
2021-03-09 13:01:58 +0100

    Preserve compatibility with Python 3.8

--- decoupled_dj/settings/production.py
+++ decoupled_dj/settings/production.py
@@ -4,6 +4,7 @@ SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 STATIC_ROOT = env("STATIC_ROOT")
 
-REST_FRAMEWORK = REST_FRAMEWORK | {
-    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"]
+REST_FRAMEWORK = {
+    **REST_FRAMEWORK,
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
