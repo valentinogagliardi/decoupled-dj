11e2c7f61eb9885c066af77c4fffc8d080c3146d
valentinogagliardi <valentino@valentinog.com>
2021-03-29 18:16:33 +0200

    Configure monitus

--- decoupled_dj/settings/production.py
+++ decoupled_dj/settings/production.py
@@ -4,9 +4,26 @@ SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 STATIC_ROOT = env("STATIC_ROOT")
 
+
 REST_FRAMEWORK = {
     **REST_FRAMEWORK,
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
 
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+
+INSTALLED_APPS = INSTALLED_APPS + ["monitus"]
+
+MIDDLEWARE = MIDDLEWARE + [
+    "monitus.middleware.FailedLoginMiddleware",
+    "monitus.middleware.Error403EmailsMiddleware",
+]
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+
+
+ADMINS = [("Your name here", "your@email.here")]
